bb294c1f2bc037336151ed2db1c8cf791df56909
Faiz U. Shaikh <fizi.live@gmail.com>
2024-08-11 21:34:00 +0500

    login
    login page

---
+++ app/data/actions/auth-actions.js
@@ -0,0 +1,14 @@
+"use server";
+
+export async function RegisterUserAction(formData, FormData){
+    console.log("Hi from use server");
+    
+    const fields = {
+        username: formData.get("email"),
+        password: formData.get("password"),
+    };
+    console.log(">>>>>>>>>>>>>>>");
+    console.log(fields);
+    console.log(">>>>>>>>>>>>>>>");
+    
+};
